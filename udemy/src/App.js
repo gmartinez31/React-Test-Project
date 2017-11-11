98bd7bffb21565521478d70149290c0e10764957
gmartinez31 <bhgustavo13@yahoo.com>
2017-11-10 20:19:40 -0600

    updated Udemy app

--- udemy/src/App.js
+++ udemy/src/App.js
@@ -5,15 +5,43 @@ import Person from './Person/Person';
 // JSX is just syntactic sugar for JavaScript, allowing you to write HTMLish code instead of nested React.createElement(...) calls.
 
 class App extends Component {
+  // state only available when 'extends Component' is used //
+  state = {
+    persons: [
+      {name: 'Gustavo', age: 26},
+      {name: 'Imelda', age: 25},
+      {name: 'Baby', age: 19},
+      {name: 'Freddy', age: 18},
+    ]
+  }
+
+  // property or method of the App class //
+  switchNameHandler = () => {
+    // console.log('Clicked bruh.');
+    // DONT DO THIS -> this.state.persons[0].name = 'Blah'
+    
+    // with setState React will recoginze the change
+    this.setState({persons: 
+      [
+        { name: 'Gustavo', age: 26 },
+        { name: 'Imelda', age: 25 },
+        { name: 'Cris', age: 19 },
+        { name: 'Freddy', age: 200 },
+      ]
+    });
+  }
+
   render() {
     // good practice to wrap everything under one root element per component used //
     return (
       <div className="App">
         <h1>First React App</h1>
-        <Person name='Gustavo' age='26' />
-        <Person name='Imelda' age='25' />
-        <Person name='Cris' age='19' />
-        <Person name='Freddy' age='18' />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {/* in this case 'this' refers to the class */}
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Hobbies: Music, Lifting, and Running</Person>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
       </div>
     );
 
--- udemy/src/Person/Person.js
+++ udemy/src/Person/Person.js
@@ -3,7 +3,12 @@ import React from 'react';
 // {} anything inside these braces react interprets as actual js not literal 'html'(jsx) so the input is dynamic rather than static
 
 const person = (props) => {
-    return <p>I'm a {props.name} and I'm {props.age} years old!</p>
+    return (
+        <div>
+            <p>I'm {props.name} and I'm {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 // same as:
 // const person = function () {
